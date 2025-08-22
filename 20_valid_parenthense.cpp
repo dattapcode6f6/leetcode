33cb9be0e6884db2ece46454b808a998e8c2be22
datngu999 <datnguyenx0@gmail.com>
2025-08-22 15:40:04 +0700

    20. Valid Parenthenses.

---
+++ 20_valid_parenthense.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        int num = s.size();
+        for(int i = 0;i < num;i++) {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                stack.push(s[i]);
+            } else {
+                if(stack.empty()) {
+                    return false;
+                }
+        
+            char top = stack.top();
+            stack.pop();
+            if(
+                (s[i]==')' && top!='(') ||
+                (s[i]=='}' && top!='{') ||
+                (s[i]==']' && top!='[')
+            ) {
+                return false;
+            }
+        }
+        }
+        return stack.empty();
+    }
+};
